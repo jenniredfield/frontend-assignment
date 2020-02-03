3e2ad9bae29e733662d2f701d6044caa9e05d4da
Jen Bird <jenniredfield@hotmail.com>
2020-02-03 23:47:40 +0000

    Finished task 5

--- src/5/index.tsx
+++ src/5/index.tsx
@@ -9,7 +9,8 @@ import "./index.less";
 class Task5 extends React.Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    animated: ""
   };
 
   handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,22 +19,48 @@ class Task5 extends React.Component {
     });
   };
 
+  handleFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      animated: e.currentTarget.name
+    });
+  };
+
+  handleBlur = () => {
+    this.setState({
+      animated: ""
+    });
+  };
+
   render() {
+    const { animated } = this.state;
+ 
     return (
-      <form>
-        <label>Email</label>
-        <input
-          name="email"
-          onChange={this.handleInputChange}
-          value={this.state.email}
-        />
-        <label>Password</label>
-        <input
-          name="password"
-          onChange={this.handleInputChange}
-          value={this.state.password}
-        />
-        <button>Login</button>
+      <form className="form">
+        <div className="form__row">
+          <label className={`form__label ${animated === 'email' ? 'form__label--animated' : ''}`}>Email</label>
+          <input
+            name="email"
+            onChange={this.handleInputChange}
+            value={this.state.email}
+            className="form__input"
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+          />
+
+        </div>
+
+        <div className="form__row">
+          <label className={`form__label ${animated === 'password' ? 'form__label--animated' : ''}`}>Password</label>
+          <input
+            name="password"
+            onChange={this.handleInputChange}
+            value={this.state.password}
+            className="form__input"
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+          />
+        </div>
+        <button className="form__button">Login</button>
       </form>
     );
   }
