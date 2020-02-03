af33c44608516e38d1537a17bd0f0fa47469cf1b
Jen Bird <jenniredfield@hotmail.com>
2020-02-03 23:42:05 +0000

    Finished task 2

--- src/2/index.tsx
+++ src/2/index.tsx
@@ -5,14 +5,20 @@ class Task2 extends React.Component {
   state = {
     value: ''
   };
+
+  handleChange = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+    this.setState({ value: target.value })
+  }
+
   render() {
     return (
       <div>
         <label>Controlled Input: </label>
-        <input />
+        <input onChange={this.handleChange} />
         <br />
         <br />
-        <p>#Show Input value here#</p>
+        <p>{this.state.value}</p>
       </div>
     );
   }
