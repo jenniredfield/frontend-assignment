5fa10145b2da4422a914d7f7169725d4f29e1b89
Jen Bird <jenniredfield@hotmail.com>
2020-02-03 23:43:11 +0000

    Finished task 3

--- src/3/index.tsx
+++ src/3/index.tsx
@@ -4,16 +4,34 @@ import * as React from "react";
 class Task3 extends React.Component {
   state = {
     inputValue: "",
-    data: [""]
+    data: ["Banana", "Apple", "Pear"]
   };
+
+  handleChange = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+    this.setState({ inputValue: target.value })
+  }
+
   render() {
+    const { inputValue, data } = this.state;
     return (
       <div>
         <label>Search Input: </label>
-        <input />
+        <input onChange={this.handleChange} />
         <br />
         <br />
-        #List goes here#
+        {
+          data.filter(x => {
+            if (!inputValue) {
+              return true;
+            }
+
+            return inputValue.toLowerCase().substring(0, inputValue.length) === x.toLowerCase().substring(0, inputValue.length);
+          }).map((value, i) => {
+            return (
+              <li key={`${value}_${i}`}>{value}</li>
+            );
+          })}
       </div>
     );
   }
